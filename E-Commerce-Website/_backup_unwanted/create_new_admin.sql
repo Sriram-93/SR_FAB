b1635e43344b2951fb96c4dac54dedd82f29492c
Sriram-93 <sriramknr63@gmail.com>
2026-03-26 22:36:58 +0530

    chore: Clean up catalog SQL from repo

--- E-Commerce-Website/_backup_unwanted/create_new_admin.sql
+++
@@ -1,7 +0,0 @@
-USE defaultdb;
-
-INSERT INTO admin (email, password, name, phone) 
-VALUES ('newadmin@gmail.com', 'admin123', 'New Admin', '9876543210')
-ON DUPLICATE KEY UPDATE email=VALUES(email);
-
-SELECT * FROM admin WHERE email = 'newadmin@gmail.com';
